c92988fec66bdefc5f82f2e291ccd0681fed17d0
002002 <liaokai@fcbox.com>
2021-03-01 16:18:42 +0800

    save

---
+++ udemy_js_advanced/02_foundation2/03_argument.js
@@ -0,0 +1,31 @@
+/** part1 */
+function plus(a, b) {
+  return a + b
+}
+console.log(plus(1, 2))
+
+/** part2 */
+function plus2(a, b) {
+  let args = Array.from(arguments)
+  return args[0] + args[1]
+}
+console.log(plus2(1, 2))
+
+/** part3 */
+function plus3(...args) {
+  return args[0] + args[1]
+}
+console.log(plus3(1, 2))
+
+/** part4 */
+function plus3({a, b}) {
+  console.log(arguments[0]);
+  return a + b
+}
+console.log(plus3({a: 1, b: 2}))
+
+/** notice1 */
+function notice() {
+  console.log(arguments);
+}
+notice()
