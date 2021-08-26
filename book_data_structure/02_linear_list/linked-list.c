28559fbec5c55918b7b9568f6774940bd1541e50
akai <liaokaigogo@foxmail.com>
2021-08-27 00:36:54 +0800

    save

--- book_data_structure/02_linear_list/linked-list.c
+++ book_data_structure/02_linear_list/linked-list.c
@@ -13,11 +13,11 @@ struct Node
 
 struct LinkedList
 {
-  Node * head = NULL;
   int length;
+  struct Node * head = NULL;
 };
 
-Status ListInit(struct LinkedList * L)
+enum Status ListInit(struct LinkedList * L)
 {
   L->head = (struct Node * )malloc(sizeof(struct Node *));
   L->head->next = NULL;
@@ -25,36 +25,47 @@ Status ListInit(struct LinkedList * L)
   return OK;
 }
 
-Status ListInsert(struct LinkedList * L, int index, ElemType e)
+enum Status ListInsert(struct LinkedList * L, int index, ElemType e)
 {
   struct Node * p = L->head;
 
   int i = 0;
-  while (p != NULL && i <= index)
+  while (p != NULL && i < index)
   {
     p = p->next;
     i++;
   }
-  
-  if (p != NULL)
-  {
-    struct Node * q = (struct Node * )malloc(sizeof(struct Node *));
-    q->data = e;
-    q->next = p->next;
-    
-  }
-  
+
+  if (!p || i >= index) return ERROR;
+
+  struct Node * q = (struct Node * )malloc(sizeof(struct Node *));
+  q->data = e;
+  q->next = p->next;
+  p->next = q;
 
   return OK;
 }
 
-
-
+void ListDisplay(struct LinkedList * L)
+{
+  struct Node * p = L->head->next;
+  while (p != NULL)
+  {
+    printf("%d ", p->data);
+    p = p->next;
+  }
+  printf("\n");
+}
 
 int main(int argc, char const *argv[])
 {
   struct LinkedList L;
   ListInit(&L);
+  ListInsert(&L, 0, 2);
+  ListInsert(&L, 0, 3);
+  ListInsert(&L, 0, 5);
+  ListInsert(&L, 4, 7);
+  ListDisplay(&L);
   
   return 0;
 }
