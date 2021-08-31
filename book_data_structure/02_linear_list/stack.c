4332d9c357c090d7654731aa229c0fd9a43e8633
akai <liaokaigogo@foxmail.com>
2021-08-31 14:48:37 +0800

    save

--- book_data_structure/02_linear_list/stack.c
+++ book_data_structure/02_linear_list/stack.c
@@ -1,8 +1,22 @@
 #include "./array.c"
 
+struct Stack
+{
+  struct ArrayList * L
+}
+
+void push(struct ArrayList * L, ElemType e)
+{
+  // L.
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+  ElemType arr[] = {};
+
+  struct ArrayList L;
+  InitList(&L, arr, sizeof(arr)/sizeof(arr[0]));
+  DisplayList(L);
+
+  return 0;
 }
