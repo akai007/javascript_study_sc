9ba5eac8422e51009982b3b235fa28054d196805
akai <liaokaigogo@foxmail.com>
2021-09-08 09:31:32 +0800

    save

--- mooc_dbms/02_0710_view.sql
+++ mooc_dbms/02_0710_view.sql
@@ -1,5 +1,5 @@
 # 视图
-
+use mooc;
 # 定义一个视图 cs_stu 为计算机系学生，通过该视图可以将Student表中 其他系的学生屏蔽掉
 CREATE VIEW cs_stu AS (
 	SELECT *
@@ -12,11 +12,12 @@ SELECT * FROM cs_stu
 
 # 定义一个视图teach 为教师任课情况，把teacher表中的个人隐私方面的信息，如工资等信息屏蔽掉，仅反映教哪门课以及学分等。
 CREATE VIEW teach AS (
-	SELECT t.name, c,name, 
-    FROM s_c sc, teacher t
-    WHERE t.tid = c.tid
+	SELECT t.name t_name, c.name c_name
+    FROM course c, teacher t
+    WHERE c.tid = t.tid
 );
-
+SELECT * FROM teach ;
+SELECT * FROM teach WHERE c_name = '数据库';
 
 
 
