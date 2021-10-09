8e986cdc41a2eae9a51aa4fe5fbf638f97393d17
akai <liaokaigogo@foxmail.com>
2021-10-09 17:17:21 +0800

    Create insertion_sort.c

---
+++ udemy_c_data_structure_algorithms/20_sort/insertion_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, j, x;
+
+    for (i = 1; i < n; i++)
+    {
+        x = arr[i];
+
+        for (j = i-1; j >= 0 && x < arr[j] ; j--)
+        {
+            arr[j+1] = arr[j]; 
+        }
+        arr[j+1] = x;
+    }
+}
+
+void displayList(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = { 5, 2, 7, 3, 2 };
+    insertionSort(&arr, sizeof(arr)/sizeof(arr[0]));
+    displayList(&arr, sizeof(arr)/sizeof(arr[0]));
+
+    return 0;
+}
