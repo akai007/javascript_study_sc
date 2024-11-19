15b729fa10c9a97a721d413ec2e8313922362763
akai888 <liaokaigogo@foxmail.com>
2024-11-19 10:28:56 +0800

    python

---
+++ python/001.py
@@ -0,0 +1,20 @@
+print('Hello Python')
+
+# String
+first_name = "Bro"
+food = "hotpot"
+
+# Int
+age = 33
+
+# Float
+gpa = 3.4
+
+# Boolean
+is_male = True
+
+print(f'My frist name is: {first_name}')
+print(f'I am {age} years old')
+print(f'May GPA is : {gpa}')
+
+print(f'I am a {'male' if is_male else 'female'}')
